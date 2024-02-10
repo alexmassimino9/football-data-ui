82cdb45b9aa99341097005ca64db04daa3268e87
Alexander Massimino <49351994+alexmassimino9@users.noreply.github.com>
2024-02-10 14:55:53 -0500

    added init context

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,3 +1,10 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useEffect, useState, useContext } from "react";
 
-const AppProvider = React.createProvider();
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  return <AppContext.Provider>{children}</AppContext.Provider>;
+};
+
+export { AppContext, AppProvider };
